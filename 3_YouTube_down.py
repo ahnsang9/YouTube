4d80c048431343e1bc133974df08bd54abbe795c
ahnsanghoon <40432401+ahnsang9@users.noreply.github.com>
2020-07-15 21:02:54 +0900

    laptop test3

--- 3_YouTube_down.py
+++ 3_YouTube_down.py
@@ -6,11 +6,11 @@ from pytube import YouTube
 
 data = pd.read_excel('melon_힙_list.xlsx')
 data = data['urls']
-
+'''
 for i in data:
-    yt = YouTube('%s',%i)
+    #yt = YouTube('%s',%i)
     yt_streams = yt.streams
     yt.streams.filter(only_audio=True)
     itag = 140
     my_stream = yt_streams.get_by_itag(itag)
-    my_stream.download('C:\\Users\안상훈\Desktop\youtube_download')
+    my_stream.download('C:\\Users\안상훈\Desktop\youtube_download')'''
