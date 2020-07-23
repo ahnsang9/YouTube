deb301891d2d2693a4f7568ffcdd2cdac9bdeeef
안 상 훈 <ahnsang9@naver.com>
2020-07-23 23:44:50 +0900

    mp3로 정상적인 변환 성공

--- __playlist_down.py
+++ __playlist_down.py
@@ -5,16 +5,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from pytube import YouTube
 import time
-import pandas as pd
 import eyed3
 import glob
 import os
+from pydub import AudioSegment
+from tqdm import tqdm
 
 id = 'ahnsang9@naver.com'
 pw = 'wpgkrmfk1!'
 
 while 1:
-    print('데탑이면 1 , 랩탑이면 2')
+    print('데탑 -> 1\n랩탑 -> 2')
     num = int(input())
     if num == 1:
         driver_path = 'C:\\Users\82105\PycharmProjects\YouTube_git2\chromedriver.exe'
@@ -22,15 +23,15 @@ while 1:
         break
     elif num == 2:
         driver_path = 'C:\\Users\안상훈\PycharmProjects\chromedriver.exe'
-        download_pate = ''
+        download_path = ''
         break
     else:
         print('???\n')
 
 options = webdriver.ChromeOptions()
 # headless 옵션 설정
-#options.add_argument('headless')
-#options.add_argument("no-sandbox")
+options.add_argument('headless')
+options.add_argument("no-sandbox")
 # 브라우저 윈도우 사이즈
 options.add_argument('window-size=1920x1080')
 # 사람처럼 보이게 하는 옵션들
@@ -72,11 +73,19 @@ k = 0
 for i in list_name:
     k += 1
     list_names.append(i.text)
-    print('%d : '%k + i.text)
-print('\n다운로드 원하는 playlist 번호를 입력하세요 : ')
-num = int(input())
-print('\n')
-driver.find_element_by_xpath('//*[@id="pageList"]/table/tbody/tr[%d]/td[2]/div/div/dl/dt/a'%num).click() #원하는 플레이리스트로 이동
+    print('%d . '%k + i.text)
+
+while 1:
+    print('\n다운로드 원하는 playlist 번호를 입력하세요')
+    num = int(input())
+    if num > 0 & num <= len(list_names):
+        break
+    else :
+        print('잘못 입력했습니다.')
+selected_list_name = list_names[num-1]
+number_of_songs = driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div[2]/div[2]/div/div/table/tbody/tr[%d]/td[3]/div/p'%num)
+number_of_songs = int(number_of_songs.text[8:-1])
+driver.find_element_by_xpath('//*[@id="pageList"]/table/tbody/tr[%d]/td[2]/div/div/dl/dt/a'%num).click() #원하는 플레이리스트로 이동driver.find_element_by_xpath('//*[@id="pageList"]/table/tbody/tr[%d]/td[2]/div/div/dl/dt/a'%num).click() #원하는 플레이리스트로 이동
 
 titles = []
 singers = []
@@ -85,22 +94,23 @@ albums = []
 page = 1
 count = 0
 id = driver.current_url[-9:]
-back = '#params%5BplylstSeq%5D='
-back2 = '&po=pageObj&startIndex='
-fixed_current_playlist_url = 'https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=%s%s%s%s'%(id,back,id,back2)
+fixed_current_playlist_url = 'https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=%s'%id+'#params%5BplylstSeq%5D=%s'%id+'&po=pageObj&startIndex='
+
+
+pbar = tqdm(total=number_of_songs,desc='플레이리스트 목록 다운중..')
 while 1:
-    driver.get(fixed_current_playlist_url + '%d' % page) #페이지 이동
+    driver.get(fixed_current_playlist_url + '%d'%page) #페이지 이동
     '''try:
         element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, "pop_isms")))
     finally:
         pass'''
-
     time.sleep(1)
-    title =  driver.find_elements_by_xpath('//*[@id="frm"]/div/table/tbody/tr/td[3]/div/div/a[1]/span')
+
+    title = driver.find_elements_by_xpath('//*[@id="frm"]/div/table/tbody/tr/td[3]/div/div/a[1]/span')
     if len(title) == 0:
         break
     singer = driver.find_elements_by_id('artistName')
-    album =  driver.find_elements_by_xpath('//*[@id="frm"]/div/table/tbody/tr/td[5]/div/div/a')
+    album = driver.find_elements_by_xpath('//*[@id="frm"]/div/table/tbody/tr/td[5]/div/div/a')
 
     for i in title:
         titles.append(i.text)
@@ -108,24 +118,17 @@ while 1:
         singers.append(i.text)
     for i in album:
         albums.append(i.text)
-    page += 50
-    count += len(title)
-    print("%d개 완료"%count)
 
-'''df = pd.DataFrame({"title" : titles,
-                   "singer" : singers,
-                   "album" : albums})
-df.to_excel("멜론플레이리스트_%s.xlsx"%list_names[num-1])
-list_name = list_names[num-1]
-driver.quit()'''
-print('\n*****playlist 다운 완료*****\n')
+    page += 50
+    pbar.update(len(title))
+pbar.close()
+
 
 driver.get('https://www.youtube.com/')
-
 action = ActionChains(driver) #단축키 보내기
 mute = 0
 urls = []
-for i in range(len(titles)):
+for i in tqdm(range(len(titles)),desc='url 복사중..'):
     element = driver.find_element_by_name("search_query") #search창 지정
     element.clear() #search창 클리어
     element.send_keys(f'{titles[i]} {singers[i]} official audio mp3')
@@ -144,37 +147,42 @@ for i in range(len(titles)):
                     mute = 1
                 if url[:29] == 'https://www.youtube.com/watch':
                     urls.append(driver.current_url) #현재 페이지 url 복사
-                    print('url 다운중...')
                     break
                 else:continue
             except:pass
 driver.quit()
 
-for i in range(len(titles)):
-    yt = YouTube(urls[i])
-    yt_streams = yt.streams
-    yt.streams.filter(only_audio=True)
-    itag = 140
-    my_stream = yt_streams.get_by_itag(itag)
 
-    # my_stream.download('C:\\Users\82105\Desktop\YouTube','%s'%yt_title) #데탑
-    my_stream.download(download_path,'%s'%titles[i]) #랩탑
-    print('%s 완료...'%titles[i])
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('같은 폴더가 있습니다')
+createFolder(download_path + '\%s'%selected_list_name)
 
-files = glob.glob(download_path + "\*.mp4")
-for x in files:
-    if not os.path.isdir(x):
-        filename = os.path.splitext(x)
-        try:
-            os.rename(x,filename[0] + '.mp3')
-        except:
-            pass
-for i in range(len(titles)):
-    song = eyed3.load("C:\\Users\82105\Desktop\YouTube\%s.mp3" % titles[i])
-    song.tag.title = ['%s' % titles[i]]
-    song.tag.artist = ['%s' % singers[i]]
-    song.tags.album = ['%s' % albums[i]]
+
+for i in tqdm(range(len(titles)), desc='mp4 다운중..'):
+    try:
+        yt = YouTube(urls[i])
+        yt_streams = yt.streams
+        yt.streams.filter(only_audio=True)
+        itag = 140
+        my_stream = yt_streams.get_by_itag(itag)
+        my_stream.download(download_path + '\%s'%selected_list_name,'%s'%titles[i])
+    except:pass
+
+
+os.chdir(download_path + '\%s'%selected_list_name)
+temp = glob.glob('*.mp4')
+for i in tqdm(range(len(temp)),desc='mp4 -> mp3 변환중..'):
+    mp3_filename = os.path.splitext(os.path.basename(temp[i]))[0] + '.mp3'
+    AudioSegment.from_file(temp[i]).export(mp3_filename, format='mp3')
+
+
+for i in range(len(titles)):  #태그 초기화
+    song = eyed3.load(download_path + "\%s\%s.mp3" %(selected_list_name, titles[i]))
+    song.tag.title = '%s' % titles[i]
+    song.tag.artist = '%s' % singers[i]
+    song.tag.album = '%s' % albums[i]
     song.tag.save()
-    print(song.tags)
-
-print('\n***** 변환완료 *****\n')
