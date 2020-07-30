9ec6612845f4482050b65d6649cc4b9d2b25fa5f
안 상 훈 <ahnsang9@naver.com>
2020-07-30 16:20:09 +0900

    .

--- __playlist_down.py
+++ __playlist_down.py
@@ -31,23 +31,22 @@ def createFolder(directory):
         print('같은 폴더가 있습니다')
 
 
-
-
-def bs_crawling(url,list):
-    def try_(munjang):
-        try:
-            munjang
-        except:
-            pass
+def bs_crawling(url, list):
     soup = BeautifulSoup(session.get(url,headers = headers).text,'html.parser')
-    try_(list[3].append(soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd:nth-child(6)')[0].text))
-    try_(list[0].append(soup.select('#downloadfrm > div > div > div.entry > div.info > div.song_name')[0].text.replace('\n','').replace('\t','').replace('\r','')[2:]))
-    try_(list[1].append(soup.select('#downloadfrm > div > div > div.entry > div.info > div.artist > a')[0].text))
-    try_(list[2].append(soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd:nth-child(2) > a')[0].text))
-    try_(list[5].append(soup.select('#d_video_summary')[0].get_text(separator='\n').replace('\t','').replace('\r','')))
-    try_(list[4].append(soup.select('#downloadfrm > div > div > div.thumb > a > img')[0]['src']))
+    title = soup.select('#downloadfrm > div > div > div.entry > div.info > div.song_name')[0].text.replace('\n','').replace('\t','').replace('\r', '').replace('19금','')[2:]
+    title = "".join(i for i in title if i not in "\/:*?<>|")
+    list[0].append(title)
+    list[1].append(soup.select('#downloadfrm > div > div > div.entry > div.info > div.artist')[0].text)
+    list[2].append(soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd:nth-child(2) > a')[0].text)
+    list[3].append(soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd:nth-child(6)')[0].text)
+    list[4].append(soup.select('#downloadfrm > div > div > div.thumb > a > img')[0]['src'])
+    try:
+        list[5].append(soup.select('#d_video_summary')[0].get_text(separator='\n').replace('\t','').replace('\r',''))
+    except:
+        list[5].append([''])  # 19금은 로그인해야 가사 나옴 (bs4로는 어렵)
 
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"}
 session = requests.Session()
 
 ID = 'ahnsang9@naver.com'
@@ -69,12 +68,8 @@ options.add_argument("lang=ko_KR")  # 사람처럼 보이게 하는 옵션들 # 
 
 driver = webdriver.Chrome(driver_path,chrome_options=options)
 before_login = len(driver.window_handles)  # 시작시 윈도우 수
-driver.get("https://accounts.kakao.com/login?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2"
-           "Fauthorize%3Fclient_id%3D6cfb479f221a5adc670fe301e1b6690c%26redirect_uri%3Dhttps%"
-           "253A%252F%252Fmember.melon.com%252Foauth.htm%26response_type%3Dcode%26state%3Dtqwv"
-           "R%2540t%252FeWbVEGGPRllAIOKVGlnYr9Vk7MMim6C6xG29W%2540ukELOWbfqLmL1bR1gkghWty%2540%"
-           "252FFIc5D8FnDybJ%2540uA%253D%253D%26encode_state%3Dtrue")  # 멜론 로그인 페이지
 
+driver.get("https://accounts.kakao.com/login?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fclient_id%3D6cfb479f221a5adc670fe301e1b6690c%26redirect_uri%3Dhttps%253A%252F%252Fmember.melon.com%252Foauth.htm%26response_type%3Dcode%26state%3DtqwvR%2540t%252FeWbVEGGPRllAIOKVGlnYr9Vk7MMim6C6xG29W%2540ukELOWbfqLmL1bR1gkghWty%2540%252FFIc5D8FnDybJ%2540uA%253D%253D%26encode_state%3Dtrue")  # 멜론 로그인 페이지
 click('/html/body/div[1]/div[2]/div/div/div/div/div[2]/form/fieldset/div[2]/div/label',1)
 driver.find_element_by_xpath('//*[@id="id_email_2"]').send_keys(ID)
 driver.find_element_by_xpath('//*[@id="id_password_3"]').send_keys(PW)
@@ -83,36 +78,30 @@ driver.find_element_by_xpath('//*[@id="login-form"]/fieldset/div[8]/button[1]').
 while 1:
     if len(driver.window_handles) == before_login + 1:
         break  # 창 하나 더 생기면 관둠
+
 driver.switch_to.window(driver.window_handles[-1])  # 새로생긴창 활성화
 click('//*[@id="gnb_menu"]/ul[2]/li[1]/a/span[2]',1)  # 마이뮤직 버튼
 click('//*[@id="conts"]/div[1]/ul/li[3]/a/span',1)  # 플레이리스트 버튼
+
 list_names = [x.text for x in driver.find_elements_by_xpath('//*[@id="pageList"]/table/tbody/tr/td[2]/div/div/dl/dt/a')]
 print(*list_names, sep='  ')
-while 1:
-    print('\n다운로드 원하는 playlist 번호를 입력하세요')
-    num = int(input())-1
-    if 0 <= num & num <= len(list_names)-1:
-        break
-    else:
-        print('잘못 입력했습니다.')
-
-
+print('\n다운로드 원하는 playlist 번호를 입력하세요')
+num = int(input())-1
 number_of_songs = int(driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div[2]/div[2]/div/div/table/tbody/tr[%d]/td[3]/div/p'%(num+1)).text[8:-1])
 click('//*[@id="pageList"]/table/tbody/tr[%d]/td[2]/div/div/dl/dt/a'%(num+1),1)  # 원하는 플레이리스트로 이동
 createFolder(download_path + '\%s'%list_names[num])
 
+
 info = [[],[],[],[],[],[]]  # 제목, 가수, 앨범, 장르, 커버사진, 가사
 url_id = driver.current_url[-9:]
 for i in tqdm(range(number_of_songs//50+1),desc='플레이리스트 정보 다운중...'):
-    driver.get('https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=%s'
-               '#params%%5BplylstSeq%%5D=%s&po=pageObj&startIndex=%d' % (url_id, url_id, 50*i+1))  # 페이지 이동
+    driver.get('https://www.melon.com/mymusic/playlist/mymusicplaylistview_inform.htm?plylstSeq=%s#params%%5BplylstSeq%%5D=%s&po=pageObj&startIndex=%d' % (url_id, url_id, 50*i+1))  # 페이지 이동
     time.sleep(1)
     song_url = ['https://www.melon.com/song/detail.htm?songId=' + x.get_attribute('href')[36:-3] for x in driver.find_elements_by_class_name('btn_icon_detail')]
-
     for j in range(len(song_url)):
         bs_crawling(song_url[j],info)
-        img_src = info[4][j]
-        urlretrieve(img_src, download_path + '\%s'%list_names[num] + '\%s.jpg'%(50*i+j))
+        img_src = info[4][50*i+j]
+        urlretrieve(img_src, download_path + '\%s'%list_names[num] + '\%s.jpg'%(info[0][50*i+j]))
 
 driver.get('https://www.youtube.com/')
 mute = 0
@@ -124,21 +113,17 @@ for i in tqdm(range(number_of_songs),desc='url 복사중..'):
     click('//*[@id="search-icon-legacy"]',1)  # 검색버튼 클릭
     click('//*[@id="thumbnail"]/yt-img-shadow',2)
     time.sleep(1)
-    yt_url_temp = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/'
-                                               'ytd-two-column-search-results-renderer/div/ytd-section-list-'
-                                               'renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-r'
-                                               'enderer[1]/div[1]/ytd-thumbnail/a').get_attribute('href')
+    yt_url_temp = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/ytd-thumbnail/a').get_attribute('href')
     yt_urls.append(yt_url_temp)
+
 driver.quit()
 
+
 for i in tqdm(range(number_of_songs), desc='mp4 다운중..'):
-    try:
-        yt = YouTube(yt_urls[i])
-        yt_streams = yt.streams
-        yt.streams.filter(only_audio=True)
-        my_stream = yt_streams.get_by_itag(140)
-        my_stream.download(download_path + '\%s' % list_names[num], '%s' % info[0][i])
-    except:pass
+    yt = YouTube(yt_urls[i])
+    yt_streams = yt.streams.filter(only_audio=True)
+    my_stream = yt_streams.get_by_itag(140)
+    my_stream.download(download_path + '\%s' % list_names[num], '%s' % info[0][i])
 
 
 os.chdir(download_path + '\%s' % list_names[num])
@@ -147,13 +132,12 @@ for i in tqdm(range(len(temp)),desc='mp4 -> mp3 변환중..'):
     mp3_filename = os.path.splitext(os.path.basename(temp[i]))[0] + '.mp3'
     AudioSegment.from_file(temp[i]).export(mp3_filename, format='mp3')
 
-
 for i in range(number_of_songs):  # 태그 초기화
     song = eyed3.load(download_path + "\%s\%s.mp3" %(list_names[num], info[0][i]))
     song.tag.title = '%s' % info[0][i]
     song.tag.artist = '%s' % info[1][i]
     song.tag.album = '%s' % info[2][i]
     song.tag.track_num = i
-    song.tag.images.set(3, open(download_path + "\%s\%d.jpg" %(list_names[num], i), 'rb').read(), 'image/jpeg')
+    song.tag.images.set(3, open(download_path + "\%s\%d.jpg" %(list_names[num], info[0][i]), 'rb').read(), 'image/jpeg')
     song.tag.lyrics.set(info[5][i])
     song.tag.save(version=eyed3.id3.ID3_V2_3)
--- test2.py
+++ test2.py
@@ -1,22 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+
 session = requests.Session()
-
-
-
-
-info = [[],[],[],[],[],[]]  # 제목, 가수, 앨범, 장르, 커버사진, 가사
-def bs_crawling(url,list):
-    soup = BeautifulSoup(session.get(url,headers = headers).text,'html.parser')
-    list[3].append(soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd:nth-child(6)')[0].text)
-    list[0].append(soup.select('#downloadfrm > div > div > div.entry > div.info > div.song_name')[0].text.replace('\n','').replace('\t','').replace('\r','')[2:])
-    list[1].append(soup.select('#downloadfrm > div > div > div.entry > div.info > div.artist > a')[0].text)
-    list[2].append(soup.select('#downloadfrm > div > div > div.entry > div.meta > dl > dd:nth-child(2) > a')[0].text)
-    list[5].append(soup.select('#d_video_summary')[0].get_text(separator='\n').replace('\t','').replace('\r',''))
-    list[4].append(soup.select('#downloadfrm > div > div > div.thumb > a > img')[0]['src'])
-
-
-a = bs_crawling('https://www.melon.com/song/detail.htm?songId=106215',info)
-a = bs_crawling('https://www.melon.com/song/detail.htm?songId=32183386',info)
-print(info)
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"}
+a = session.get('https://www.youtube.com/channel/UC3SyT4_WLHzN7JmHQwKQZww', headers=headers)
+print(a)
+soup = BeautifulSoup(a.text, 'html.parser')
+print(soup.select('#subscriber-count'))
